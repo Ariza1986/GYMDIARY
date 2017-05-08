ce440eaf3cdea99cc528b8b1387325bf9684db34
郭建宏 <ArizaKuo@kuochienhungmac.local>
2017-05-08 23:01:22 +0800

    17/05/08

--- GYMDIARY/ViewController.swift
+++ GYMDIARY/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //let myUserDefaults = UserDefaults.standard
+        
+        //catch date YYYYMMdd
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYYMMdd"
+        print(formatter.string(from: now))
+        
+        DispatchQueue.main.async {
+        //print("This is run on the main queue, after the previous code in outer block")
+            self.typeYourWeight()
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func typeYourWeight() {
+        let alertController = UIAlertController(title: "Waring!!",
+                                                message: "What's your Weight today?",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        /*
+        alertController.addTextField { (textField1) in
+            textField1.text = "Weight Today"
+            textField1.keyboardType = UIKeyboardType.numbersAndPunctuation
+        }
+        
+        alertController.addTextField { (textField2) in
+            textField2.text = "Body Fat % Today"
+            textField2.keyboardType = UIKeyboardType.numbersAndPunctuation
+        }
+        */
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style:UIAlertActionStyle.default,
+                                                handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
